97dc3442bffb38d22982e047dd7f4712bdd2269a
Amit <amitrozen2015@gmail.com>
2022-10-18 23:01:56 +0300

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import './navbar.scss'
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import { useState } from 'react';
+
+const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    window.onscroll = () => {
+        setIsScrolled(window.scrollY === 0 ? false : true)
+        return () => (window.onscroll = null)
+    }
+    return (
+        <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+            <div className="container">
+                <div className="left">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+                        alt="Netflix logo"
+                    />
+                    <span>Homepage</span>
+                    <span>Series</span>
+                    <span>Movies</span>
+                    <span>New and Popular</span>
+                    <span>My List</span>
+                </div>
+                <div className="right">
+                    <SearchIcon className='icon'></SearchIcon>
+                    <span>KID</span>
+                    <NotificationsIcon className='icon'></NotificationsIcon>
+                    <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+                    <div className="profile">
+                        <ArrowDropDownIcon className='icon'></ArrowDropDownIcon>
+                        <div className="options">
+                            <span>Settings</span>
+                            <span>Logout</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/home/Home.jsx
@@ -0,0 +1,26 @@
+import Navbar from '../components/navbar/Navbar'
+import './home.scss'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar/>
+      <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" width="100%"/>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+    </div>
+  )
+}
+
+export default Home
