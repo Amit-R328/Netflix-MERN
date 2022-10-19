ac575d4fabba4cb820419ebc5ed21514df20f6df
Amit <amitrozen2015@gmail.com>
2022-10-19 03:47:21 +0300

    added featured

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from '../components/featured/Featured'
 import Navbar from '../components/navbar/Navbar'
 import './home.scss'
 
@@ -5,20 +6,7 @@ const Home = () => {
   return (
     <div className='home'>
       <Navbar/>
-      <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" width="100%"/>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloremque similique amet vitae et perferendis, eaque neque! Voluptatibus facilis recusandae eos tenetur blanditiis impedit, nostrum fugit temporibus expedita suscipit? Voluptatem?</p>
+      <Featured type="movie"/>
     </div>
   )
 }
